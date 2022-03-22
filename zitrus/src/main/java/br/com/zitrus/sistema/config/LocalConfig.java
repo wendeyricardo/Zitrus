704b21a7225f538935ac9a84694d49ea0a2e5a89
wendey <wendeyricardo@gmail.com>
2022-03-21 22:03:31 -0300

    adicionando uma instancia de cliente - usuário: zitrino senha: venhaserfeliz

---
+++ zitrus/src/main/java/br/com/zitrus/sistema/config/LocalConfig.java
@@ -0,0 +1,36 @@
+package br.com.zitrus.sistema.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.zitrus.sistema.domain.Clientes;
+import br.com.zitrus.sistema.service.ClientesService;
+import br.com.zitrus.sistema.service.ClientesServiceImpl;
+
+@Configuration
+@Profile("local")
+public class LocalConfig {
+	
+	@Autowired
+	ClientesServiceImpl dao;
+
+	@Bean
+	public void startDB() {
+		Clientes c1 = new Clientes();
+		c1.setNome("Zitrus");
+		c1.setCpf(123456);
+		c1.setCidade("Joinville ");
+		c1.setEmail("wendey@zitrus.com");
+		c1.setCep("89203210");
+		c1.setEndereco("Atiradores");
+		c1.setEstado("SC");
+		c1.setNumero(54);
+		c1.setUsuario("zitrino");
+		c1.setSenha("“venhaserfeliz”");
+		c1.setSexo("Masculino");
+		c1.setRua("Xavantes");
+		dao.salvar(c1);
+	}
+}
